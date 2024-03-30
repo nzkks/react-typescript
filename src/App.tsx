53dac13c14dfad5e59f9c94da22c8abcaa7e46c0
nzkks <kkshanthosh@gmail.com>
2024-03-31 10:12:14 +1300

    props with object

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
 import { Greet } from './components/props/Greet';
+import { Person } from './components/props/Person';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  };
+
   return (
     <>
       <Greet name="NZKKS" messageCount={20} isLoggedIn={true} />
+      <Person name={personName} />
     </>
   );
 }
