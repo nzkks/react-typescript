76c98f94dba5581da814d82769d22cec1c048aba
nzkks <kkshanthosh@gmail.com>
2024-04-01 08:40:30 +1300

    useContext Future Value - improve the code with type assertion

--- src/components/context/User.tsx
+++ src/components/context/User.tsx
@@ -5,26 +5,22 @@ export const User = () => {
   const userContext = useContext(UserContext);
 
   const handleLogin = () => {
-    if (userContext) {
-      userContext?.setUser({
-        name: 'NZKKS',
-        email: 'nzkks@example.com'
-      });
-    }
+    userContext.setUser({
+      name: 'NZKKS',
+      email: 'nzkks@example.com'
+    });
   };
 
   const handleLogout = () => {
-    if (userContext) {
-      userContext?.setUser(null);
-    }
+    userContext.setUser(null);
   };
 
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogout}>Logout</button>
-      <div>User name is {userContext?.user?.name}</div>
-      <div>User email is {userContext?.user?.email}</div>
+      <div>User name is {userContext.user?.name}</div>
+      <div>User email is {userContext.user?.email}</div>
     </div>
   );
 };
--- src/components/context/UserContext.tsx
+++ src/components/context/UserContext.tsx
@@ -14,7 +14,7 @@ type UserContextType = {
   setUser: React.Dispatch<React.SetStateAction<AuthUser | null>>;
 };
 
-export const UserContext = createContext<UserContextType | null>(null);
+export const UserContext = createContext({} as UserContextType);
 
 export const UserContextProvider = ({ children }: UserContextProviderProps) => {
   const [user, setUser] = useState<AuthUser | null>(null);
