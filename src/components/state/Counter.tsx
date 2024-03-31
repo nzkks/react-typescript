9aee00d47154d7e1a6dd2bac6c3f064f291fbe91
nzkks <kkshanthosh@gmail.com>
2024-03-31 20:15:49 +1300

    useReducer Strict Action Types - part 2

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -8,10 +8,16 @@ const initialState = {
   count: 0
 };
 
-type CounterAction = {
+type UpdateAction = {
   type: 'increment' | 'decrement';
   payload: number;
 };
+
+type ResetAction = {
+  type: 'reset';
+};
+
+type CounterAction = UpdateAction | ResetAction; // descriminated unions. recommended approach for typing the useReducer
 
 const reducer = (state: CounterState, action: CounterAction) => {
   switch (action.type) {
@@ -19,6 +25,8 @@ const reducer = (state: CounterState, action: CounterAction) => {
       return { count: state.count + action.payload };
     case 'decrement':
       return { count: state.count - action.payload };
+    case 'reset':
+      return initialState;
 
     default:
       return state;
@@ -33,6 +41,7 @@ export const Counter = () => {
       <div style={{ marginRight: 20 }}>Count: {state.count}</div>
       <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>Decrement 10</button>
       <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>Increment 10</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </div>
   );
 };
