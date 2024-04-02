9eef1a6bb9683cc1ab95580b6b1e45848996f2da
nzkks <kkshanthosh@gmail.com>
2024-04-02 19:30:27 +1300

    useRef with NONNULL assertion

--- src/components/ref/DomRef.tsx
+++ src/components/ref/DomRef.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useRef } from 'react';
 
 export const DomRef = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null!); // If we are sure the reference never null when we access it, we can add NONNULL assertion when invoking useRef
 
   useEffect(() => {
-    inputRef.current?.focus();
+    inputRef.current.focus(); // When the NONNULL assertion is in place, we can remove the optional chaining
   }, []);
 
   return (
