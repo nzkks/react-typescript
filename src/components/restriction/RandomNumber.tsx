b54d93784ce52cc0d8eb91e0599e3d1d1ccbbafe
nzkks <kkshanthosh@gmail.com>
2024-04-03 21:28:04 +1300

    Restricting Props

--- src/components/restriction/RandomNumber.tsx
+++ src/components/restriction/RandomNumber.tsx
@@ -1,9 +1,26 @@
-type RandomNumberProps = {
+type RandomNumberType = {
   value: number;
-  isPositive?: boolean;
-  isNegative?: boolean;
-  isZero?: boolean;
 };
+
+type PositiveNumber = RandomNumberType & {
+  isPositive: boolean;
+  isNegative?: never;
+  isZero?: never;
+};
+
+type NegativeNumber = RandomNumberType & {
+  isNegative: boolean;
+  isPositive?: never;
+  isZero?: never;
+};
+
+type Zero = RandomNumberType & {
+  isZero: boolean;
+  isPositive?: never;
+  isNegative?: never;
+};
+
+type RandomNumberProps = PositiveNumber | NegativeNumber | Zero;
 
 export const RandomNumber = ({ value, isPositive, isNegative, isZero }: RandomNumberProps) => {
   return (
